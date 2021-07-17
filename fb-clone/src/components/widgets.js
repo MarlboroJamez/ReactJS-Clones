f06a59c24140d2a55d4a9f689be424c464c1c86f
JamesHRYWilliams <53170261+JamesHRYWilliams@users.noreply.github.com>
2021-07-17 14:22:37 +0200

    fb-clone

--- fb-clone/src/components/widgets.js
+++ fb-clone/src/components/widgets.js
@@ -4,6 +4,16 @@ import '../css/widgets.css';
 function widgets() {
     return (
         <div className="widgets">
+            <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FZLaner%2Fvideos%2F1000654080711222%2F&show_text=true&width=560&t=0"
+                title="Clever Programmer Fb Page"
+                width="250"
+                height="100%"
+                style={{ border: 'none', overflow: 'hidden' }}
+                scrolling="no"
+                frameborder="0"
+                allowtransparency="true"
+                allow="encrypted-media"
+            ></iframe>
         </div>
     )
 }
