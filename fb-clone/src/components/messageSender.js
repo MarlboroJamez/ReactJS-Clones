d37b522c12776a5f985803f6747d1252d564fef8
JamesHRYWilliams <53170261+JamesHRYWilliams@users.noreply.github.com>
2021-07-18 09:13:56 +0200

    fb-clone

--- fb-clone/src/components/messageSender.js
+++ fb-clone/src/components/messageSender.js
@@ -3,15 +3,15 @@ import '../css/messageSender.css';
 import { Avatar } from '@material-ui/core';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
-import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import { useStateValue } from '../components/stateProvider';
 import db from '../components/firebase';
 import firebase from 'firebase';
 
 function MessageSender() {
     const [{ user }, dispatch] = useStateValue()
-    const [input, setInput] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
+    const [input, setInput] = useState('')
+    const [imageUrl, setImageUrl] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -28,34 +28,33 @@ function MessageSender() {
         setInput('')
         setImageUrl('')
     }
-
-    return <div className="messageSender">
-            <div className="messageSender__top">
-                <Avatar/>
+    return (
+        <div className="messageSender">
+            <div class="messageSender__top">
+                <Avatar src={user.photoURL} />
                 <form>
-                    <input value={input} onChange={(e) => setInput(e.target.value)} type="text" className="messageSender__input" placeholder={`What's on your mind, ${user.displayName}?`}/>
+                    <input value={input} onChange={(e) => setInput(e.target.value)}
+                        placeholder={`What's on your mind, ${user.displayName}?`} />
+
                     <input placeholder="Image Url (optional) " value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
-                    <button onclick={handleSubmit} type="submit"></button>
+
+                    <button type="submit" onClick={handleSubmit}>Hidden Submit</button>
                 </form>
             </div>
-            
-            <div className="messageSender__button">
-                <div className="messageSender__option">
-                    <VideocamIcon style={{ color: "red"}}/>
+            <div class="messageSender__bottom">
+                <div class="messageSender__option">
+                    <VideocamIcon style={{ color: "red" }} />
                     <h3>Live Video</h3>
-                </div>
-
-                <div className="messageSender__option">
-                    <PhotoLibraryIcon style={{ color: "green"}}/>
+                </div><div class="messageSender__option">
+                    <PhotoLibraryIcon style={{ color: "green" }} />
                     <h3>Photo/Video</h3>
-                </div>
-
-                <div className="messageSender__option">
-                    <InsertEmoticonIcon style={{ color: "orange"}}/>
+                </div><div class="messageSender__option">
+                    <InsertEmoticonIcon style={{ color: "orange" }} />
                     <h3>Feeling/Activity</h3>
                 </div>
             </div>
         </div>
+    )
 }
 
 export default MessageSender
--- fb-clone/src/components/post.js
+++ fb-clone/src/components/post.js
@@ -7,44 +7,45 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import NearMeIcon from '@material-ui/icons/NearMe';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-function post({profilePic, image, username, timestamp, message}) {
-    return <div className="post">
-            <div className="post__top">
-                <Avatar src={profilePic} className="post__avatar"/>
-                <div className="post__topInfo">
+function Post({ profilePic, image, username, timestamp, message }) {
+    return (
+        <div className='post'>
+            <div class="post__top">
+                <Avatar src={profilePic} className='post__avatar' />
+                <div class="post__topInfo">
                     <h3>{username}</h3>
                     <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
                 </div>
+            </div>
 
-                <div className="post__bottom">
-                    <p>{message}</p>
+            <div class="post__bottom">
+                <p>{message}</p>
+            </div>
+
+            <div class="post__image">
+                <img src={image} alt="" />
+            </div>
+
+            <div class="post__options">
+                <div class="post__option">
+                    <ThumbUpIcon />
+                    <p>Like</p>
                 </div>
-
-                <div className="post__image">
-                    <img src={image} alt=""/>
+                <div class="post__option">
+                    <ChatBubbleOutlineOutlinedIcon />
+                    <p>Comment</p>
                 </div>
-
-                <div className="post__options">
-                    <div className="post__option">
-                        <ThumbUpIcon/>
-                        <p>Like</p>
-                    </div>
-                    <div class="post__option">
-                        <ChatBubbleOutlineOutlinedIcon />
-                        <p>Comment</p>
-                    </div>
-                    <div class="post__option">
-                        <NearMeIcon />
-                        <p>Share</p>
-                    </div>
-                    <div class="post__option">
-                        <AccountCircleIcon />
-                        <ExpandMoreIcon />
-                    </div>
+                <div class="post__option">
+                    <NearMeIcon />
+                    <p>Share</p>
+                </div>
+                <div class="post__option">
+                    <AccountCircleIcon />
+                    <ExpandMoreIcon />
                 </div>
             </div>
-    </div>
-    
+        </div>
+    )
 }
 
-export default post
+export default Post
