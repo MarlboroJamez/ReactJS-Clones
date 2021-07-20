cce42ebd7d8369977c2640974ef7380023439ae7
JamesHRYWilliams <53170261+JamesHRYWilliams@users.noreply.github.com>
2021-07-20 07:44:54 +0200

    nf-clone

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Row from './components/row';
+import requests from './components/request';
+import Banner from './components/banner';
+import Nav from './components/nav';
 
 function App() {
   return (
     <div className="app">
-        <Row title='NETFLIX ORIGINALS'/>
-        <Row title='Trending Now'/>
+        <Nav/>
+        <Banner/>
+
+        <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
