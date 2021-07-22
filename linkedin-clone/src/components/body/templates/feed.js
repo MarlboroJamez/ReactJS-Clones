e90491f09f3c3bb4a3dc41769bfc3cf921c6c690
JamesHRYWilliams <53170261+JamesHRYWilliams@users.noreply.github.com>
2021-07-22 12:39:54 +0200

    linkedin-clone

--- linkedin-clone/src/components/body/templates/feed.js
+++ linkedin-clone/src/components/body/templates/feed.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import '../../../styles/feed.css';
+import CreateIcon from '@material-ui/icons/Create';
+import InputOption from './inputOption';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import Post from './post';
 
 function Feed() {
     return (
         <div className="feed">
             <div className="feed__inputContainer">
-                
+                <div className="feed__input">
+                    <CreateIcon/>
+                    <form>
+                        <input type="text" />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#C7A33C"/>
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
+                </div>
             </div>
+
+            <Post name="James Williams" description="just a test" message="Hello fellow linkedin's" photoUrl=""/>
         </div>
     )
 }
---
+++ linkedin-clone/src/components/body/templates/post.js
@@ -0,0 +1,35 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import '../../../styles/post.css';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+import InputOption from './inputOption';
+
+function Post({ name, description, message, photoUrl }) {
+    return (
+        <div className="post">
+            <div className="post__header">
+                <Avatar src={photoUrl}/>
+                <div className="post__info">
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+
+            <div className="post__body">
+                <p>{message}</p>
+            </div>
+
+            <div className="post__buttons">
+                <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray"/>
+                <InputOption Icon={ChatOutlinedIcon} title="Like" color="gray"/>
+                <InputOption Icon={ShareOutlinedIcon} title="Like" color="gray"/>
+                <InputOption Icon={SendOutlinedIcon} title="Like" color="gray"/>
+            </div>
+        </div>
+    )
+}
+
+export default Post
